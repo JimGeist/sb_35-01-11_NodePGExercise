ebaf0b700c52cdcf859fc21a6a64fe33b5e66e4e
Jim Geist <info@jimgeist.com>
2021-06-08 23:01:38 -0400

    v0.0.3 BizTime company REST-ful backend API server for a company / invoice tracker. Technology Stack is Node.js and Postgres on the backend.

--- helperFx.js
+++ helperFx.js
@@ -25,14 +25,28 @@ function validateRequired(fieldList, source, allRequired = true) {
 
     const fields = {}
     let allValid = true;
+    let ctr = 0;
 
     let errorValidation = "";
     let errorDelim = "";
+    debugger;
     for (const key of fieldList) {
-        console.log(`key: ${key}, req.body.${key} = '${source[key]}'`)
-        if (source[key]) {
-            // key exists with a value.
-            fields[key] = source[key].trim();
+        // if (source[key]) works . . . until source[key] refers to a boolean :/
+        //  a boolean set to false no less. Soo the below code is used in place.
+        //  I wish I can say it came out of my brain, but it came out of vscode 
+        //  when you type 'for' and select forin from the suggestions. 
+        // I imagine typeof(source[key]) === "string" is not the best either.
+        if (Object.hasOwnProperty.call(source, key)) {
+            // key exists 
+            if (typeof (source[key]) === "string") {
+                fields[key] = source[key].trim();
+            } else {
+                fields[key] = source[key]
+            }
+
+            // keep a count of how many keys were found. We will need to
+            //  know when no keys exist when all are not required.
+            ctr++;
         } else {
             if (allRequired) {
                 // key is missing and all keys are required
@@ -48,6 +62,7 @@ function validateRequired(fieldList, source, allRequired = true) {
         return {
             allValid: allValid,
             fields: fields,
+            nbrOfFields: ctr,
             error: "",
         };
     } else {
@@ -63,15 +78,17 @@ function validateRequired(fieldList, source, allRequired = true) {
 }
 
 
-function prepareCompanyInsert(requiredKeys, requestBody) {
-    /*  prepareCompanyInsert prepares Company data from the request for eventual
+function prepareInsertData(requiredKeys, optionalKeys, requestBody) {
+    /*  prepareInsertData prepares Company or Invoice data from the request for eventual
          insert into the database.
-        Route adds a new company is added by using JSON inputs for code, name, and description
-         Returns new company object {company: {code, name, description}}
+        requiredKeys, array, name of each required (not nullable) field. The requestBody
+         oject is checked for these fields.
+        optionalKeys, arra, name of each optional (nullable) field. These fields will not
+         cause an error if missing.
+        requestBody, object, the request object body that should contain the keys and values
+         for the fields listed in requiredKeys.
     
     */
-
-    // debugger
 
     // make sure non-blank values exist for code and name.
     // Order of requiredKeys in the list must match the order expected by the insert!!
@@ -95,7 +112,6 @@ function prepareCompanyInsert(requiredKeys, requestBody) {
     let insertNames = "";
     let insertDelim = ""
     for (const key of requiredKeys) {
-        console.log(`for: ctr=${ctr}, key=${key}, resultsValidation.fields[key]= ${resultsValidation.fields[key]}`)
         insertValues.push(resultsValidation.fields[key])
         // build the argument string, $1, $2, ... for the insert by adding to 
         //  insertArgs string each time through the loop.
@@ -105,25 +121,36 @@ function prepareCompanyInsert(requiredKeys, requestBody) {
         insertDelim = ", ";
     }
 
-    // do we have a description? It is an outlier since it is not required.
-    if (requestBody.description) {
-        let description = (requestBody.description).trim();
-        if (description.length > 0) {
-            // we have a description. put it in the value list.
-            insertValues.push(description);
-            // update insertArgs to show a $ctr and insertNames to include
-            //  descripiton. ctr and delim will have correct values for 
-            //  appending to the argument string.
-            insertArgs = `${insertArgs}${insertDelim}$${ctr}`;
-            insertNames = `${insertNames}${insertDelim}description`;
+    if (optionalKeys.length > 0) {
+        // check whether a value exists for the optional key in requestBody.
+        let tester;
+        for (const key of optionalKeys) {
+            if (Object.hasOwnProperty.call(optionalKeys, key)) {
+                // optionalKeys were not validated so we need to check them
+                if (typeof (requestBody[key]) === "string") {
+                    // We have a string. trim and save it when it has a non-zero length.
+                    tester = requestBody[key].trim();
+                    if (tester.length > 0) {
+                        insertValues.push(tester);
+                        // the args, names, and ctr stuff are duplicated because
+                        //  after trimming, we may have an empty string which we
+                        //  can ignore.
+                        insertArgs = `${insertArgs}${insertDelim}$${ctr}`;
+                        insertNames = `${insertNames}${insertDelim}${key}`;
+                        ctr++;
+                        insertDelim = ", ";
+                    }
+                } else {
+                    insertValues.push(requestBody[key]);
+                    insertArgs = `${insertArgs}${insertDelim}$${ctr}`;
+                    insertNames = `${insertNames}${insertDelim}${key}`;
+                    ctr++;
+                    insertDelim = ", ";
+                }
+            }
+
         }
-
     }
-    console.log(`insertArgs=${insertArgs}`);
-    console.log(`insertValues=${insertValues}`);
-    console.log(`insertNames=${insertNames}`);
-
-    // const results = await dbInsert(insertArgs, insertValues, "code, name, description", "companies");
 
     return {
         success: true,
@@ -137,7 +164,127 @@ function prepareCompanyInsert(requiredKeys, requestBody) {
 }
 
 
+function prepareUpdateData(requestKeys, requestBody) {
+    /*  prepareUpdateData prepares Company data from the request for eventual
+         update of an existing company.
+
+        requestKeys, array, is the list of fields we need to check for in requestBody.
+         This is an update, so all fields are optional, but at least one is required.
+
+        Update request are issued regardless of whether the data has changed. 
+
+        When no issues occurred while validating fields, function returns
+            {
+                success: true,
+                updateData: {
+                    argumentsNbr: [array of $1, $1, ..n],
+                    argumentsName: [array of field names],
+                    argumentsValues: [array of field values]
+                }
+            }
+        When an error occurred (no requestKeys),
+            {
+                success: false,
+                error: `Field(s) ${fields} are needed for an update. No fields were provided.`
+            }
+    */
+
+    // debugger
+
+    // validate the request keys. Third parm is false because all requestKeys are NOT required.
+    const resultsValidation = validateRequired(requestKeys, requestBody, false);
+
+    // NOTE: no need to check whether resultsValidation.allValid === false
+    //  since all fields listed in requestKeys are not required.
+
+    // resultsValidation.fields has the set of fields and cleaned values as
+    //  key: value. For the update, we need to split out the keys, values, 
+    //  and build a $ paramter list.
+    if (resultsValidation.nbrOfFields === 0) {
+        // no fields were in request body.
+        // build field list for message that resembles
+        //  "field1 or field2" OR "field1, field2, or field3"
+        let fields = "";
+        let fieldsDelim = "";
+        let ctr = 1;
+        for (const field of requestKeys) {
+            fields = `${fields}${fieldsDelim}'${field}'`;
+            if (ctr + 1 === requestKeys.length) {
+                // add ' or ' to the delimitor. This is the delimiter before the last
+                //  field. Handle the two spaces when delimitor already is set to ", ".
+                fieldsDelim = (`${fieldsDelim} or `).replace("  ", " ");
+            } else {
+                fieldsDelim = ", ";
+            }
+            ctr++;
+        }
+        return {
+            success: false,
+            error: `Field(s) ${fields} are needed for an update. No fields were provided.`
+        }
+    }
+
+    const insertValues = [];
+    const fieldValues = [];
+    const fieldNames = [];
+    const updateArgs = [];
+    let ctr = 1;
+    for (const key in resultsValidation.fields) {
+        // console.log(`for: ctr=${ctr}, key=${key}, resultsValidation.fields[key]= ${resultsValidation.fields[key]}`)
+        fieldValues.push(resultsValidation.fields[key]);
+        fieldNames.push(key);
+        updateArgs.push(`$${ctr}`);
+        ctr++;
+    }
+
+    return {
+        success: true,
+        updateData: {
+            argumentsNbr: updateArgs,
+            argumentsName: fieldNames,
+            argumentsValues: fieldValues
+        }
+    };
+
+}
+
+
+function checkNumbers(inNumber) {
+    /*  checks the values in inNumber to make sure it is indeed a number. 
+       
+        When inNumber is a number, function returns:
+            {
+            "numberIsValid": true,
+            "validatedNumber": inNumber // where inNumber is returned as a number, not a string.
+            }
+        When a non-number is encountered, function returns:
+            {
+            "numberIsValid": false,
+            "message" = 'nums=${inNumber}' is invalid. '${inNumber}' is not a number.`
+            }
+  
+    */
+
+    const results = {};
+
+    let nbrText = inNumber * 1;
+    if (isNaN(nbrText)) {
+        results["numberIsValid"] = false;
+        results["message"] = `'${inNumber}' is invalid. '${inNumber}' is not a number.`;
+        return results;
+    }
+
+    results["numberIsValid"] = true;
+    results["validatedNumber"] = inNumber * 1;
+
+    return results;
+
+}
+
+
 module.exports = {
-    prepareCompanyInsert: prepareCompanyInsert,
-    validateRequired: validateRequired
+    checkNumbers: checkNumbers
+    , prepareInsertData: prepareInsertData
+    , prepareUpdateData: prepareUpdateData
+    , validateRequired: validateRequired
 };
--- routes/companies.js
+++ routes/companies.js
@@ -3,8 +3,8 @@
 const db = require("../db");
 const express = require("express");
 const ExpressError = require("../expressError");
-const { prepareCompanyInsert } = require("../helperFx");
-const { dbInsert } = require("../dbFunctions");
+const { prepareInsertData, prepareUpdateData } = require("../helperFx");
+const { dbDelete, dbInsert, dbSelect, dbUpdate } = require("../dbFunctions");
 const { response } = require("express");
 const router = express.Router();
 
@@ -36,7 +36,25 @@ router.get("/:code", async function (req, res, next) {
         );
 
         if (results.rows.length > 0) {
-            return res.json({ company: results.rows });
+            // get the invoices for the company.
+            const selectData = {
+                criteria: 'comp_code = $1',
+                criteriaValues: [inCode],
+                selectFields: "id, amt"
+            }
+
+            const resultsInvoices = await dbSelect(selectData, "invoices");
+            if (resultsInvoices.success) {
+                results.rows[0]["invoices"] = resultsInvoices.sqlReturn;
+            }
+
+
+
+
+
+
+
+            return res.json({ company: results.rows[0] });
         } else {
             let errorNotFound = new Error(`A company was not found for code '${inCode}'.`);
             errorNotFound.status = 404;
@@ -50,13 +68,6 @@ router.get("/:code", async function (req, res, next) {
 });
 
 
-
-
-
-
-
-
-
 /** POST {/companies}/ ; return new company object {company: {code, name, description}} **/
 router.post("/", async function (req, res, next) {
     // Route adds a new company is added by using JSON inputs for code, name, and description
@@ -66,7 +77,7 @@ router.post("/", async function (req, res, next) {
     // make sure non-blank values exist for code and name.
     // Order of requiredKeys in the list must match the order expected by the insert!!
     const requiredKeys = ["code", "name"];
-    const resultsPreparation = prepareCompanyInsert(requiredKeys, req.body);
+    const resultsPreparation = prepareInsertData(requiredKeys, ["description"], req.body);
     if (resultsPreparation.success === false) {
         // An error occurred while validating the data when success is false.
         // const errorValidation = new Error(resultsPreparation["error"]);
@@ -75,103 +86,65 @@ router.post("/", async function (req, res, next) {
         errorValidation.status = 400;
         return next(errorValidation);
     }
-    // debugger;
-    console.log(`resultsPreparation.argumentsNbr=${resultsPreparation.insertData.argumentsNbr}`);
-    console.log(`resultsPreparation.argumentsValues=${resultsPreparation.insertData.argumentsValues}`);
-    console.log(`resultsPreparation.argumentsName=${resultsPreparation.insertData.argumentsName}`);
 
     const resultsInsert = await dbInsert(resultsPreparation.insertData, "companies");
 
-
     if (resultsInsert.success) {
-        return res.json({ company: resultsInsert.insertReturn });
+        // successful insert - return results
+        return res.json({ company: resultsInsert.sqlReturn });
     } else {
         const errorInsert = new Error(resultsInsert.error.message);
-        errorInsert.status = 500;
+        errorInsert.status = 400;
         return next(errorInsert);
     }
-
-    // if (resultsValidation.allValid === false) {
-    //     // bad request (400) -- all fields are needed when adding a company.
-    //     const errorValidation = new Error(resultsValidation["error"]);
-    //     errorValidation.status = 400;
-    //     return next(errorValidation);
-    // }
-
-    // const insertValues = [];
-    // let ctr = 1;
-    // let insertArgs = "";
-    // let insertDelim = ""
-    // for (const key of requiredKeys) {
-    //     console.log(`for: ctr=${ctr}, key=${key}, resultsValidation.fields[key]= ${resultsValidation.fields[key]}`)
-    //     insertValues.push(resultsValidation.fields[key])
-    //     // build the argument string, $1, $2, ... for the insert by adding to 
-    //     //  insertArgs string each time through the loop.
-    //     insertArgs = `${insertArgs}${insertDelim}$${ctr}`;
-    //     ctr++;
-    //     insertDelim = ", ";
-    // }
-
-    // // do we have a description? It is an outlier since it is not required.
-    // if (req.body.description) {
-    //     let description = (req.body.description).trim();
-    //     if (description.length > 0) {
-    //         // we have a description. put it in the value list.
-    //         insertValues.push(description);
-    //         // update insertArgs to show a $ctr. ctr and delim will 
-    //         //  have correct values for appending to the argument string.
-    //         insertArgs = `${insertArgs}${insertDelim}$${ctr}`;
-    //     }
-
-    // }
-
-    // try {
-    //     const result = await db.query(`
-    //         INSERT INTO companies 
-    //         VALUES (${insertArgs}) 
-    //         RETURNING code, name, description 
-    //     `, insertValues);
-    //     return res.json({ company: result.rows[0] })
-
-    // } catch (error) {
-    //     return next(error);
-    // }
 
 });
 
 
-// router.post("/", async function (req, res, next) {
-//     // Route adds a new company is added by using JSON inputs for code, name, and description
-//     // Returns new company object {company: {code, name, description}}
-//     // debugger
+/** PUT {/companies}/[code] ; return edited company object {company: {code, name, description}} **/
+router.put("/:code", async function (req, res, next) {
+    // Existing company idenified by code is upadated JSON inputs for name and description
+    // Returns edited company object {company: {code, name, description}} when successful or
+    //  404 / Company not found when the code was not found.
 
-//     // make sure non-blank values exist for code and name.
+    // make sure non-blank values exist for code and name.
+    // Order of requiredKeys in the list must match the order expected by the insert!!
+    const requiredKeys = ["name", "description"];
+    const resultsPreparation = prepareUpdateData(requiredKeys, req.body);
 
-//     const requiredKeys = ["code", "name"];
-//     const resultsValidation = validateRequired(requiredKeys, req.body);
+    if (resultsPreparation.success === false) {
+        // An error occurred while validating the data when success is false.
+        // bad request (400) -- fields are missing.
+        const errorValidation = new Error(resultsPreparation.error);
+        errorValidation.status = 400;
+        return next(errorValidation);
+    }
 
-//     if (resultsValidation.allValid === false) {
-//         // bad request (400) -- all fields are needed when adding a company.
-//         const errorValidation = new Error(resultsValidation["error"]);
-//         errorValidation.status = 400;
-//         return next(errorValidation);
-//     }
+    // Update of a company is by the primary key 'code'. value is 
+    const criteria = {
+        pk: "code",
+        value: req.params.code
+    }
+    const resultsUpdate = await dbUpdate(criteria, resultsPreparation.updateData, "companies");
 
-//     let { code, name, description } = resultsValidation.fields;
-//     console.log(`resultsValidation.fields=${resultsValidation.fields}`);
-//     try {
-//         const result = await db.query(`
-//             INSERT INTO companies 
-//             VALUES ($1, $2, $3) 
-//             RETURNING code, name, description 
-//         `, [code, name, description]);
-//         return res.json({ company: result.rows[0] })
+    if (resultsUpdate.success) {
+        // successful update - return results
+        return res.json({ company: resultsUpdate.sqlReturn });
+    } else {
+        if (resultsUpdate.error.message === "not found") {
+            const errorUpdate = new Error(`A company was not found for code '${req.params.code}'.`);
+            errorUpdate.status = 404;
+            return next(errorUpdate);
+        } else {
+            const errorUpdate = new Error(resultsUpdate.error.message);
+            errorUpdate.status = 400;
+            return next(errorUpdate);
+        }
 
-//     } catch (error) {
-//         return next(error);
-//     }
+    }
 
-// });
+});
+
 
 // /** PUT {/companies}/[code] ; return edited company object {company: {code, name, description}} **/
 // router.put("/:code", async function (req, res next) {
@@ -202,11 +175,37 @@ router.post("/", async function (req, res, next) {
 
 
 /** DELETE {/companies}/[code] ; return deleted message {company: {status: "deleted"}} **/
-// Deletes a company idenified by code.
-// Returns {company: {status: "deleted"}} upon successful deletion or
-//  404 / Company not found when the code was not found.
+router.delete("/:code", async function (req, res, next) {
+    // Deletes a company idenified by code.
+    // Returns {company: {status: "deleted"}} upon successful deletion or
+    //  404 / Company not found when the code was not found.
 
+    const code = req.params.code;
+    const deleteData = {
+        criteria: "code=$1",
+        criteriaValues: [code],
+        argumentsName: "code, name, description",
+    }
+    const resultsDelete = await dbDelete(deleteData, "companies");
 
+    if (resultsDelete.success) {
+        // return res.json({ deleted: resultsDelete.deleted });
+        return res.json(resultsDelete.message);
+
+    } else {
+        if (resultsDelete.error.message === "not found") {
+            const errorDelete = new Error(`A company was not found for code '${code}'.`);
+            errorDelete.status = 404;
+            return next(errorDelete);
+        } else {
+            const errorDelete = new Error(resultsDelete.error.message);
+            errorDelete.status = 400;
+            return next(errorDelete);
+        }
+
+    }
+
+});
 
 
 module.exports = router;
